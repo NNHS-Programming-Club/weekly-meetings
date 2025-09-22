3524d7b545e51a9bc579068f4976210d45c4e881
Heona <75399515+heonaliu@users.noreply.github.com>
2025-09-22 22:45:50 +0000

    add variables and modify to be more concise

--- heona-liu/09-22-25/herding.py
+++ heona-liu/09-22-25/herding.py
@@ -5,10 +5,13 @@ cow1, cow2, cow3 = listCow[0], listCow[1], listCow[2]
 minimum = 0
 maximum = 0
 
-if ((cow3 - cow2) == 1) and ((cow2 - cow1) == 1): #all numbers together => min = 0
+d1 = cow3 - cow2
+d2 = cow2 - cow1
+
+if (d1 == 1 and d2 == 1): #all numbers together => min = 0
     minimum = 0
 
-elif (((cow3 - cow2)) == 2 or ((cow2 - cow1)) == 2): #check 2 numbers have a difference of 2 =>  min = 1
+elif (d1 == 2 or d2 == 2): #check 2 numbers have a difference of 2 =>  min = 1
     minimum = 1
 
 else:
@@ -16,9 +19,9 @@ else:
 
 #MAXIMUM
 
-if ((cow3 - cow2) > (cow2 - cow1)):
-    maximum = (cow3 - cow2) - 1 # -1 because you are looking for the numbers between the largest dist between 2 numbers
+if (d1 > d2):
+    maximum = d1 - 1 # largest dist between 2 numbers
 else:
-    maximum = (cow2-cow1) - 1
+    maximum = d2 - 1
 
-print(maximum)
+print(minimum, maximum)
