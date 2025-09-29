be16cd3095f3fce8d38467b295701944b4e814b8
billjoe0459 <billjoe0459@gmail.com>
2025-09-29 20:54:03 +0000

    I made a game

--- yakov-bogorad/9-29-28/game.py
+++ yakov-bogorad/9-29-28/game.py
@@ -1,1 +1,14 @@
-print("hi")
+import random
+
+secret = random.randint(1, 10)
+rounds = 10
+x = 1.5
+
+while x != secret :
+    x = int(input())
+    if x > secret:
+        print("too high")
+    if x < secret:
+        print("too low")
+
+print("you won!")
