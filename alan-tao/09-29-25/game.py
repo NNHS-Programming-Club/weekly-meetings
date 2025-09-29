013af208633e23b390ea9345293fce783e16f115
Alan Tao <47901465+alantao5056@users.noreply.github.com>
2025-09-29 20:51:16 +0000

    finished game

--- alan-tao/09-29-25/game.py
+++ alan-tao/09-29-25/game.py
@@ -1,1 +1,17 @@
-print("hi")
+import random
+
+secret = random.randint(1, 10)
+
+turns = 0
+while True:
+    guess = int(input("What is your guess? "))
+    turns += 1
+    if (guess > secret):
+        print("Too Big")
+    elif (guess < secret):
+        print("Too Small")
+    else:
+        print("You got it!")
+        print("It took you " + str(turns) + " turns.")
+        break
+
