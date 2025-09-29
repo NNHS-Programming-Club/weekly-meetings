df7ebd4f4fea8210ac6f7819298772858f6524aa
Raj Kasarle <106451458+HenryK-codes@users.noreply.github.com>
2025-09-29 21:05:55 +0000

    Finished game, made on mobile lol

--- raj-kasarle/09-29-25/bank-game.py
+++ raj-kasarle/09-29-25/bank-game.py
@@ -1,1 +1,22 @@
-print("henlo")
+import random
+
+print("I am thinking of a number between 1 and 10.")
+
+secret = random.randint(1, 10)
+rounds = int(input("How many rounds?\n"))
+
+guess = int(input((str(rounds) + " rounds. Guess the number.")))
+tries = 0
+
+while tries < rounds:
+    tries +=1
+    if guess == secret:
+        print("You got my number!")
+        break
+    elif guess > secret:
+        print("Lower.")
+    elif guess < secret:
+        print("Higher.")
+
+print("The number was " + secret + ".")
+print("Turns used: " + tries)
