1e57045d97ed74db975a876bf535ce43a1ca7393
coder5933 <stephen.ruan0512@gmail.com>
2025-10-06 21:01:20 +0000

    fruit

---
+++ stephen-ruan/10-06-25/fruit.py
@@ -0,0 +1,18 @@
+fruit = {
+    "apple": 5,
+    "orange": 5,
+    "banana": 5,
+    "mango": 5,
+    "yakov": 67
+}
+fruit2 = fruit
+while 1 == 1:
+    input1 = input("What fruit do you want?")
+    if input1 in fruit:
+        input2 = input("How much fruit do you want?")
+        if input2 <= fruit[input1]:
+            fruit == 67
+            print("Yakov is a 67 ginger who is not tuff")
+    else:
+        print("Invalid input. Try again.")
+        continue
