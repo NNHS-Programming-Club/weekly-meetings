0e0e4ff4a10921d106e11cf1052a5965353d695c
Raj Kasarle <106451458+HenryK-codes@users.noreply.github.com>
2025-10-06 23:43:34 +0000

    test 3; trying out challenge 3

--- raj-kasarle/10-06-25/triangle.py
+++ raj-kasarle/10-06-25/triangle.py
@@ -5,3 +5,12 @@ for i in range(0, shape1):
 shape2 = int(input("Let's make another one.\nHow tall should it be?\n"))
 for i in range(0, shape2):
     print("*"*(shape2-i))
+
+shape3 = int(input("Let's try a square outline.\nSide length?\n"))
+for i in range(0, shape3):
+    if i == 0 or i == shape3-1:
+        print("*"*shape3)
+    else:
+        print("*", end="")
+        print(" "*(shape3-2), end="")
+        print("*", end="\n")
