6f85a3c9f5dc2c4dffe08524f8f7fbced6bc772d
Fishlover-hub <15354523310@163.com>
2025-10-20 21:05:30 +0000

    plaguecheat.cc

---
+++ Yiming-Sun/10-20-25/classes.py
@@ -0,0 +1,13 @@
+class Note:
+    value = 0
+    next = None
+    def __init__ (self, value):
+        self.value = value
+
+def makeLL(arr):
+    head = Node(arr[0])
+    previuos = head
+
+    for i in range(1,len(arr)):
+        newHead = Node(arr[i])
+        
