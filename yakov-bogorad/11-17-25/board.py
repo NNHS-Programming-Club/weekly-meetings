14e9bc2eca0d08af2d05495c1961e710b2cf9bf8
billjoe0459 <billjoe0459@gmail.com>
2025-11-17 21:34:37 +0000

    omi tried to hack my folder

---
+++ yakov-bogorad/11-17-25/board.py
@@ -0,0 +1,10 @@
+a=int(input())
+for _ in range(a):
+    N, M = list(map(int, input().split()))
+    grid = []
+    for i in range(N):
+        grid.append(list(map(int, input().split())))
+
+
+
+print()
